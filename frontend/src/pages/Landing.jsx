867be7177a68fbd1859d593743b72a908317a9ae
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-08-14 19:56:08 +0530

    Hero section

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../components/landing/Hero'
+
+const Landing = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  )
+}
+
+export default Landing
